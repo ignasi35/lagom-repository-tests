29dcde37827b9a4fae4240c990173aa749988175
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-05-29 11:35:21 +0200

    Another attempt with manual build of dependencies.

--- hello-impl/src/test/scala/com/example/hello/impl/HelloRepoSpec.scala
+++ hello-impl/src/test/scala/com/example/hello/impl/HelloRepoSpec.scala
@@ -1,28 +1,15 @@
 package com.example.hello.impl
 
-import akka.{ Done, NotUsed }
-import com.example.hello.api.{ GreetingMessageChanged, _ }
-import com.lightbend.lagom.scaladsl.api.Service.{ named, pathCall, topic }
-import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
-import com.lightbend.lagom.scaladsl.api.broker.Topic
-import com.lightbend.lagom.scaladsl.api.broker.kafka.{ KafkaProperties, PartitionKeyStrategy }
-import com.lightbend.lagom.scaladsl.api._
-import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
-import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraPersistenceComponents
-import com.lightbend.lagom.scaladsl.persistence.{ PersistentEntityRegistry, ReadSide }
-import com.lightbend.lagom.scaladsl.server.{ LagomApplication, LagomServer, LocalServiceLocator }
-import com.lightbend.lagom.scaladsl.testkit.ServiceTest
-import com.softwaremill.macwire.wire
-import com.typesafe.config.ConfigFactory
+import akka.actor.ActorSystem
+import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraSession
+import com.typesafe.config.{ Config, ConfigFactory }
 import org.scalatest.{ AsyncWordSpec, BeforeAndAfterAll, Matchers }
-import play.api.Configuration
-import play.api.libs.ws.WSClient
-import play.api.libs.ws.ahc.AhcWSComponents
 
 class HelloRepoSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
 
-  // Disabled Lagom's ConfigSessionProvided based on ServiceLocators.
-  // See https://www.lagomframework.com/documentation/current/scala/ProductionOverview.html#Using-static-Cassandra-contact-points
+  // Overriding the session-provider is required because Lagom sets the default
+  // value to Lagom's `ServiceLocatorServiceProvider` which is not what we want
+  // in these tests.
   val configString =
     """
       |cassandra.default {
@@ -31,42 +18,15 @@ class HelloRepoSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
       |}
     """.stripMargin
 
-  private val server = ServiceTest.startServer(
-    ServiceTest.defaultSetup
-  ) { ctx =>
-    new  LagomApplication(ctx)
-      with MyReadSideComponents
-      with AhcWSComponents
-      with ProvidesAdditionalConfiguration {
-      override def additionalConfiguration: AdditionalConfiguration =
-        super.additionalConfiguration ++
-          Configuration(ConfigFactory.parseString(configString))
-
-      override lazy val lagomServer = serverFor[EmptyService](wire[EmptyServiceImpl])
-      override def persistentEntityRegistry: PersistentEntityRegistry = ???
-      override def serviceLocator: ServiceLocator = NoServiceLocator
-    }
-  }
-
-
-  val client = server.serviceClient.implement[HelloService]
-
-  override protected def afterAll() = server.stop()
+  val config: Config = ConfigFactory.load().withFallback(ConfigFactory.parseString(configString))
+  val system = ActorSystem("MyRepo-spec", Some(config))
+  val casSession :CassandraSession = new CassandraSession(system)
+  val myRepo = new MyRepo(casSession)
 
   "MyRepo" should {
     "have a cassandraSession injected" in {
-      server.application.myRepo.exists should be(true)
+      myRepo.exists should be(true)
     }
   }
 }
 
-trait EmptyService extends Service {
-  override final def descriptor = {
-    import Service._
-    named("empty")
-  }
-}
-
-class EmptyServiceImpl extends EmptyService{
-
-}
